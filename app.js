2674d1ab52d295620793b40f327a2c2ad079a0ba
Jeff French <jeff.french@moonswitch.com>
2012-08-27 17:20:47 -0400

    Updated to run on Heroku.

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ express.compiler.compilers.less.compile = function(str, fn){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
